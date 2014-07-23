340fae87834dc2e97145e91d9a86d140f056a4c1
Stan Thornhill <vassago360@yahoo.com>
2014-07-23 09:31:04 -0500

    foo

--- server.py
+++ server.py
@@ -74,62 +74,62 @@ def main():
     #pickle.dump( badURLs, open('badWikipediaArticles.p', 'wb'))
     #import pdb ; pdb.set_trace()
     uploadBadWikipediaArticles()
-    try:
-        while(True):
-            #process badURLs
-            subprocess.call(r'scp st1298@eros.cs.txstate.edu:transportedBW*.p ./ ', shell=True)
-            subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm transportedBW*.p  ', shell=True)
-            processedSomething = False
-            for fileName in os.listdir(os.getcwd()):
-                if "transportedBW" in fileName:
-                    print("processing " + fileName)
-                    processedSomething = True
-                    processBadWikipediaArticles(fileName)
-                    os.remove(os.getcwd() + "/" + fileName)
-            if processedSomething:
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu cat < badWikipediaArticles.p ">" badWikipediaArticles1.p  ', shell=True)
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm badWikipediaArticles.p  ', shell=True)
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu mv badWikipediaArticles1.p badWikipediaArticles.p ', shell=True)
-            #process transportedTR*.db
-            subprocess.call(r'scp st1298@eros.cs.txstate.edu:transportedTR*.db ./ ', shell=True)
-            subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm transportedTR*.db  ', shell=True)
-            processedSomething = False
-            for fileName in os.listdir(os.getcwd()):
-                if "transportedTR" in fileName:
-                    print("processing " + fileName)
-                    processedSomething = True
-                    processTransportedTR(fileName)
-                    os.remove(os.getcwd() + "/" + fileName)
-            if processedSomething or True:
-            #if processedSomething:
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu cat < taxonomyRelations.db ">" taxonomyRelations1.db  ', shell=True)
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm taxonomyRelations.db  ', shell=True)
-                    subprocess.call(r'ssh st1298@eros.cs.txstate.edu mv taxonomyRelations1.db taxonomyRelations.db ', shell=True)
-            time.sleep(2)
-            conn2 = sqlite3.connect("taxonomyRelations.db")
-            c2 = conn2.cursor(cursor)
-            c2.execute("pragma foreign_keys = ON")
-            if random.choice([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]):
-                c2.printDB()
-                c2.close()
-                conn2.commit()
-            elif random.choice([0,0,0,0,0,1]):
-                print("removing unused items in database...")
-                c2.removeItemsNotBeingUsed()
-                c2.close()
-                conn2.commit()
+    #try:
+    while(True):
+        #process badURLs
+        subprocess.call(r'scp st1298@eros.cs.txstate.edu:transportedBW*.p ./ ', shell=True)
+        subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm transportedBW*.p  ', shell=True)
+        processedSomething = False
+        for fileName in os.listdir(os.getcwd()):
+            if "transportedBW" in fileName:
+                print("processing " + fileName)
+                processedSomething = True
+                processBadWikipediaArticles(fileName)
+                os.remove(os.getcwd() + "/" + fileName)
+        if processedSomething:
+                subprocess.call(r'ssh st1298@eros.cs.txstate.edu cat < badWikipediaArticles.p ">" badWikipediaArticles1.p  ', shell=True)
+                subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm badWikipediaArticles.p  ', shell=True)
+                subprocess.call(r'ssh st1298@eros.cs.txstate.edu mv badWikipediaArticles1.p badWikipediaArticles.p ', shell=True)
+        #process transportedTR*.db
+        subprocess.call(r'scp st1298@eros.cs.txstate.edu:transportedTR*.db ./ ', shell=True)
+        subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm transportedTR*.db  ', shell=True)
+        processedSomething = False
+        for fileName in os.listdir(os.getcwd()):
+            if "transportedTR" in fileName:
+                print("processing " + fileName)
+                processedSomething = True
+                processTransportedTR(fileName)
+                os.remove(os.getcwd() + "/" + fileName)
+        if processedSomething or True:
+        #if processedSomething:
                 subprocess.call(r'ssh st1298@eros.cs.txstate.edu cat < taxonomyRelations.db ">" taxonomyRelations1.db  ', shell=True)
                 subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm taxonomyRelations.db  ', shell=True)
                 subprocess.call(r'ssh st1298@eros.cs.txstate.edu mv taxonomyRelations1.db taxonomyRelations.db ', shell=True)
-            else:
-                print("sleeping...")
-                c2.close()
-                conn2.commit()
-                time.sleep(15)
-            shutil.copyfile("taxonomyRelations.db", "backUpTaxonomy.db")
+        time.sleep(2)
+        conn2 = sqlite3.connect("taxonomyRelations.db")
+        c2 = conn2.cursor(cursor)
+        c2.execute("pragma foreign_keys = ON")
+        if random.choice([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]):
+            c2.printDB()
+            c2.close()
+            conn2.commit()
+        elif random.choice([0,0,0,0,0,1]):
+            print("removing unused items in database...")
+            c2.removeItemsNotBeingUsed()
+            c2.close()
+            conn2.commit()
+            subprocess.call(r'ssh st1298@eros.cs.txstate.edu cat < taxonomyRelations.db ">" taxonomyRelations1.db  ', shell=True)
+            subprocess.call(r'ssh st1298@eros.cs.txstate.edu rm taxonomyRelations.db  ', shell=True)
+            subprocess.call(r'ssh st1298@eros.cs.txstate.edu mv taxonomyRelations1.db taxonomyRelations.db ', shell=True)
+        else:
+            print("sleeping...")
+            c2.close()
+            conn2.commit()
+            time.sleep(15)
+        shutil.copyfile("taxonomyRelations.db", "backUpTaxonomy.db")
 
-    except Exception as e:
-        processMainLoopException(e)
+    #except Exception as e:
+    #   processMainLoopException(e)
 
 if __name__ == "__main__":
     main()
